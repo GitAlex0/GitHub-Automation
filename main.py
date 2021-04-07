aeacf9585a204ff877e4a268b4fc9e3bb55450c6
GitAlex0 <alexander.petersdorf@gmail.com>
2021-04-07 11:12:27 +0200

    Updated Stuff

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 
-print("----------------------------------")
+print("-"*30)
 print("Started the Github automation")
 print("What's next?")
 print("1: Just Login")
 print("2: Create a new Repo")
 print("3: Delete a Repo")
-print("----------------------------------")
+print("-"*30)
 option = str(input())
 if option == "1":
 	os.system("github-login.py")
@@ -16,8 +16,8 @@ elif option == "3":
 	print("Work in Progress!")
 	os.system("main.py")
 else:
-	print("----------------------------------")
+	print("-"*30)
 	print("This is not an valid option!")
 	print("Try Again")
-	print("----------------------------------")
+	print("-"*30)
 	os.system("main.py")
